ad94113f4b0cd9e2f9e76e35e4a065733b0e995d
Jérémie Fouché <jeremie.fouche@gmail.com>
2024-05-16 21:52:17 +0200

    fixed angle calculation

--- src/math.rs
+++ src/math.rs
@@ -1,50 +1,52 @@
 use bevy::{
-    math::{primitives::Direction3d, Vec3},
+    math::{Vec2, Vec3Swizzles},
     transform::components::Transform,
 };
 
 pub trait SignedAngle {
     /// get the signed angle between `self` and `rhs`
-    fn signed_angle_between(&self, rhs: &Self) -> f32;
+    fn signed_angle_with(&self, rhs: Self) -> f32;
 }
 
-impl SignedAngle for Direction3d {
-    fn signed_angle_between(&self, rhs: &Self) -> f32 {
-        assert!(self.y == 0.0 && rhs.y == 0.0);
-        (**self).signed_angle_between(rhs)
-    }
-}
-
-impl SignedAngle for Vec3 {
-    fn signed_angle_between(&self, rhs: &Self) -> f32 {
-        assert!(self.y == 0.0 && rhs.y == 0.0, "{self}, {rhs}");
-        self.cross(*rhs).dot(Vec3::Y).atan2(self.dot(*rhs))
+impl SignedAngle for Vec2 {
+    fn signed_angle_with(&self, rhs: Self) -> f32 {
+        rhs.x.atan2(rhs.y) - self.x.atan2(self.y)
     }
 }
 
 impl SignedAngle for Transform {
-    fn signed_angle_between(&self, rhs: &Self) -> f32 {
-        let direction = rhs.translation.horizontal() - self.translation.horizontal();
-        direction.signed_angle_between(&self.forward().horizontal())
+    fn signed_angle_with(&self, rhs: Self) -> f32 {
+        let self_direction = *self.forward();
+        let direction_to_rhs = rhs.translation - self.translation;
+        self_direction.xz().signed_angle_with(direction_to_rhs.xz())
     }
 }
 
-pub trait Flatten {
-    fn horizontal(self) -> Self;
-}
+#[cfg(test)]
+mod test {
+    use std::f32::consts::{FRAC_PI_2, FRAC_PI_4, PI};
 
-impl Flatten for Direction3d {
-    fn horizontal(self) -> Self {
-        Direction3d::new((*self).horizontal()).unwrap()
+    use super::*;
+
+    fn eq_f32(x: f32, y: f32) -> bool {
+        (x - y).abs() < f32::EPSILON
     }
-}
 
-impl Flatten for Vec3 {
-    fn horizontal(self) -> Self {
-        Vec3 {
-            x: self.x,
-            y: 0.0,
-            z: self.z,
-        }
+    fn eq_angle(x: f32, y: f32) -> bool {
+        eq_f32(x, y) || eq_f32(x, y + 2.0 * PI) || eq_f32(x, y - 2.0 * PI)
+    }
+
+    macro_rules! assert_eq_angle {
+        ($x:expr, $y:expr) => {
+            assert!(eq_angle($x, $y), "{} != {}", $x, $y);
+        };
+    }
+
+    #[test]
+    fn test_angle() {
+        assert_eq_angle!(Vec2::X.signed_angle_with(Vec2::X), 0.0);
+        assert_eq_angle!(Vec2::X.signed_angle_with(Vec2::NEG_X), PI);
+        assert_eq_angle!(Vec2::X.signed_angle_with(Vec2::Y), -FRAC_PI_2);
+        assert_eq_angle!(Vec2::X.signed_angle_with(Vec2::new(2.0, -2.0)), FRAC_PI_4);
     }
 }
